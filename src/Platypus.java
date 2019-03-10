b79ac5fc5f77f781f82549fccdbaef2ec2a26b0d
League Student <league@iMac-4.attlocal.net>
2019-03-10 14:28:29 -0700

    Module 1

---
+++ src/Platypus.java
@@ -0,0 +1,8 @@
+
+public class Platypus {
+	private String name;
+
+  	void sayHi(String name){
+        	System.out.println("The platypus " + name + " is smarter than your average platypus.");
+  	}
+}
