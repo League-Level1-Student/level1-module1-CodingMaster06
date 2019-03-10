255410aa6e608ae23e28b91db0a081885e712769
League Student <league@iMac-4.attlocal.net>
2019-03-10 14:29:04 -0700

    module 1

--- src/RaceCarRunner.java
+++ src/RaceCarRunner.java
@@ -1,13 +1,13 @@
-
+import javax.swing.JOptionPane;
 
 public class RaceCarRunner {
 	public static void main(String[] args) {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar RC = new RaceCar();
 		// 2. Print the RaceCar's position in the race
-
+		JOptionPane.showMessageDialog(null, "The race car's position is 1st.");
 		// 3. Crash the RaceCar
 		
 		// 4. If the car is damaged. Bring it in for a pit stop.
